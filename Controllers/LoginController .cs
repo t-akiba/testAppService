c3a1a5b12f50de083a05a64e4cb2c134cea212e5
t-akiba <86754577+t-akiba@users.noreply.github.com>
2021-07-08 14:40:30 +0900

    new-2

---
+++ Controllers/LoginController .cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tm2TestAppService.Models;
+
+namespace Tm2TestAppService.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Login
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            //POST処理から直接戻り値を画面に反映させる
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/LoginModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tm2TestAppService.Models
+{
+    public class LoginModel
+    {
+        /// <summary>
+        /// ログインID
+        /// </summary>
+        [Display(Name = "ログインID")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// ログインパスワード
+        /// </summary>
+        [Display(Name = "パスワード")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// このアカウントを記憶する
+        /// </summary>
+        [Display(Name = "このアカウントを記憶する")]
+        public bool RememberMe { get; set; }
+    }
+}
